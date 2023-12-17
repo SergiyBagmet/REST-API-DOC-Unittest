7752224acb81e288860c01952c576f7a965333f7
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-12-17 15:48:19 +0200

    routers contact base pass

---
+++ src/routes/contacts.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, HTTPException, Depends, status, Path, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database.db import get_db
+from src.repository import contacts as repositories_contacts
+from src.schemas.contacts import ContactSchema, ContactUpdateSchema, ContactResponse
+
+router = APIRouter(prefix='/contacts', tags=['contacts'])
+
+
+@router.get("/")
+async def get_contacts():
+    pass
+
+
+@router.get("/{contact_id}")
+async def get_contact():
+    pass
+
+
+@router.post("/")
+async def create_contact():
+    pass
+
+
+@router.put("/{contact_id}")
+async def update_contact():
+    pass
+
+
+@router.delete("/{contact_id}")
+async def delete_contact():
+    pass
---
+++ src/schemas/contacts.py
@@ -0,0 +1,12 @@
+
+
+class ContactSchema:
+    pass
+
+
+class ContactUpdateSchema:
+    pass
+
+
+class ContactResponse:
+    pass
