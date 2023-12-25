93b27dff4e2968d01c61d5b1d6234fa06f276f59
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-12-25 12:26:58 +0200

    CORS

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from fastapi import FastAPI
 from src.routes import contacts, auth, users
 from utils import health_checker
 from utils.limiter import register_startup_event_limiter
-from utils.cors import configure_cors
+from utils.cors import configure_cors, register_user_agent_ban_middleware
 
 app = FastAPI()
 
 register_startup_event_limiter(app)
 configure_cors(app)
+register_user_agent_ban_middleware(app)
 
 app.include_router(auth.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
--- utils/cors.py
+++ utils/cors.py
@@ -1,14 +1,36 @@
-from fastapi import FastAPI
+import re
+import typing as t
+
+from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 
 def configure_cors(app: FastAPI):
-    origins = ["*"]
+    origins = ["http://localhost:3000", "http://localhost:8000"]
 
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
         allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+        allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+        allow_headers=["Authorization", "Content-Type", "X-Requested-With"],
     )
+
+
+user_agent_ban_list = [r"Googlebot", r"Python-urllib"]  # TODO add more and make in file with interface add/remove
+
+
+def register_user_agent_ban_middleware(app: FastAPI):
+    @app.middleware("http")
+    async def user_agent_ban_middleware(request: Request, call_next: t.Callable):
+        # print(request.headers.get("Authorization"))
+        user_agent = request.headers.get("user-agent")
+        banned_user_agent_patterns = [re.compile(ban_pattern) for ban_pattern in user_agent_ban_list]
+        if any(pattern.match(user_agent) for pattern in banned_user_agent_patterns):
+            return JSONResponse(
+                status_code=status.HTTP_403_FORBIDDEN,
+                content={"detail": "You are banned"},
+            )
+        response = await call_next(request)
+        return response
